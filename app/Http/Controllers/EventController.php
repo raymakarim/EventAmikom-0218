e98eeeeb071823fa4c64446159a2aeb482156536
Ray <roidm1337@gmail.com>
2026-04-17 13:13:29 +0700

    Tugas 2 masbro

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function show()
+    {
+        return view('event-detail');
+    }
+
+    public function checkout()
+    {
+        return view('checkout');
+    }
+
+    public function ticket()
+    {
+        return view('ticket');
+    }
+
+    public function indexAdmin()
+    {
+        return view('admin/events');
+    }
+
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\TransactionController;
+
+
 
 Route::get('/tentang', function () {
     return '<h1>Ini adalah halaman tentang aplikasi event hub</h1>';
@@ -21,3 +27,19 @@ Route::get('/katalog', function () {
 Route::get('/bantuan', function () {
     return view('bantuan');
 });
+
+// Rute User Area
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
+Route::get('/checkout', [EventController::class,'checkout'])->name('checkout');
+Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
+
+
+// Rute Admin area
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+Route::get('/', [DashboardController::class,'index'])->name('dashboard');
+Route::get('/events', [EventController::class,'indexAdmin'])->name('events.index');
+Route::get('/transactions', [TransactionController::class,'index'])->name('transaction.index');
+// dan seterusnya...
+});
+
